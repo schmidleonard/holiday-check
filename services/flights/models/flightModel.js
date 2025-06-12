9655e60cafbabe9204028690257f3075634ecf8b
Leonard Schmid <leonardschmid96@gmail.com>
2025-06-12 18:22:10 +0200

    changed flight service structure

---
+++ services/flights/models/flightModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const flightModelSchema = mongoose.Schema({
+  flightNumber: { type: String, required: true, unique: true },
+  departure: {
+    airportCode: { type: String, required: true },
+    city: { type: String },
+    country: { type: String }
+  },
+  destination: {
+    airportCode: { type: String, required: true },
+    city: { type: String },
+    country: { type: String }
+  },
+  aircraft: { type: String },
+  airline: { type: String, required: true },
+  departure_time: { type: Date, required: true },
+  scheduled_time: { type: Date, required: true },
+  price: { type: Number, required: true },
+  available_seats: { type: Number },
+}, { timestamps: true });
+
+module.exports = mongoose.model('flightModel', flightModelSchema);
